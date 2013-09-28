f582108d4c6abfbe560ed70e8db531e986d37441
franck <carnet.franck.paul@gmail.com>
2013-09-28 13:56:06 +0200

    Minor CSS fix, add success message on CSS save

--- index.php
+++ index.php
@@ -13,30 +13,30 @@
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
 $css_file = dirname(__FILE__).'/css/admin.css';
-$css_content = '';
-$css_writeable = false;
+$css_content = file_get_contents($css_file);
+$css_writeable = file_exists($css_file) && is_writable($css_file) && is_writable(dirname($css_file));
 
-try
-{
-	$css_content = file_get_contents($css_file);
-	$css_writeable = file_exists($css_file) && is_writable($css_file) && is_writable(dirname($css_file));
-	# Write file
-	if (!empty($_POST['css_content']))
+if (!empty($_POST['css'])) {
+	try
 	{
-		$css_content = $_POST['css_content'];
-		$fp = @fopen($css_file,'wb');
-		if (!$fp) {
-			throw new Exception(sprintf(__('Unable to write file %s. Please check your css folder permissions of this plugin.'),$css_file));
-		} else {
-			fwrite($fp,$css_content);
-			fclose($fp);
-
+		# Write file
+		if (!empty($_POST['css_content']))
+		{
+			$css_content = $_POST['css_content'];
+			$fp = @fopen($css_file,'wb');
+			if (!$fp) {
+				throw new Exception(sprintf(__('Unable to write file %s. Please check your css folder permissions of this plugin.'),$css_file));
+			} else {
+				fwrite($fp,$css_content);
+				fclose($fp);
+				http::redirect($p_url.'&css=1');
+			}
 		}
 	}
-}
-catch (Exception $e)
-{
-	$core->error->add($e->getMessage());
+	catch (Exception $e)
+	{
+		$core->error->add($e->getMessage());
+	}
 }
 ?>
 
@@ -57,6 +57,9 @@ echo dcPage::breadcrumb(
 		__('System') => '',
 		'<span class="page-title">'.__('Tidy administration settings').'</span>' => ''
 	));
+if (!empty($_GET['css'])) {
+	dcPage::success(__('CSS supplemental rules updated'));
+}
 ?>
 
 <div id="css-editor">
@@ -69,7 +72,7 @@ echo dcPage::breadcrumb(
 	if ($css_writeable)
 	{
 		echo
-		'<p><input type="submit" name="write" value="'.__('Save').' (s)" accesskey="s" /> '.
+		'<p><input type="submit" name="css" value="'.__('Save').' (s)" accesskey="s" /> '.
 		$core->formNonce().
 		'</p>';
 	}
