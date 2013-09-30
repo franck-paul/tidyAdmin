ea8292a0a0b545c1d8dcbc670a01ce3063131f5a
franck <carnet.franck.paul@gmail.com>
2013-09-30 14:38:39 +0200

    Tabs, table

--- index.php
+++ index.php
@@ -17,8 +17,10 @@ if (!defined('DC_CONTEXT_ADMIN')) { return; }
 $css_file = dirname(__FILE__).'/css/admin.css';
 $css_content = file_get_contents($css_file);
 $css_writeable = file_exists($css_file) && is_writable($css_file) && is_writable(dirname($css_file));
+$part = '';
 
 if (!empty($_POST['css'])) {
+	$part = 'css-editor';
 	try
 	{
 		# Write file
@@ -96,6 +98,9 @@ if (is_dir($iconsets_root) && is_readable($iconsets_root)) {
 }
 $dump = ob_get_clean();
 
+if ($part == '') {
+	$part = !empty($_GET['part']) && $_GET['part'] == 'iconset' ? 'iconset' : 'css-editor';
+}
 ?>
 
 <html>
@@ -106,6 +111,7 @@ $dump = ob_get_clean();
 	<link rel="stylesheet" type="text/css" href="index.php?pf=tidyAdmin/codemirror.css" />
 	<script type="text/JavaScript" src="index.php?pf=tidyAdmin/codemirror/codemirror.js"></script>
 	<script type="text/JavaScript" src="index.php?pf=tidyAdmin/codemirror/css.js"></script>
+	<?php echo dcPage::jsPageTabs($part); ?>
 </head>
 
 <body>
@@ -126,48 +132,12 @@ if ($dump != '') {
 }
 ?>
 
-<div id="iconset">
-<?php
-	echo
-	'<form id="iconset-form" action="'.$p_url.'" method="post">'.
-	'<fieldset><legend>'.__('Iconset management').'</legend>';
-	// Display list of iconset
-	if (count($iconsets)) {
-		$nb_actives = $nb_inactives = 0;
-		echo '<ul>';
-		foreach ($iconsets as $k => $v) {
-			echo
-			'<li>'.
-				__('name:').' '.$v['name'].' '.
-				__('fname:').' '.$v['fname'].' '.
-				__('enabled:').' '.($v['enabled'] ? __('yes') : __('no')).' '.
-				__('freadme:').' '.$v['freadme'].
-			'</li>';
-			if ($v['enabled']) {
-				$nb_actives++;
-			} else {
-				$nb_inactives++;
-			}
-		}
-		echo '</ul>';
-		echo
-		'<p>'.
-		'<input type="submit" name="enable" value="'.__('Enable').'" '.($nb_inactives ? '' : 'disabled="disabled"').' />'.' '.
-		'<input type="submit" name="disable" value="'.__('Disable').'" '.($nb_actives ? '' : 'disabled="disabled"').' />'.
-		'</p>';
-	}
-	echo
-	'<p>'.$core->formNonce().'</p>'.
-	'</fieldset>'.
-	'</form>';
-?>
-</div>
-<div id="css-editor">
+<div id="css-editor"  class="multi-part" title="<?php echo __('Supplemental CSS editor'); ?>">
+<h3 class="out-of-screen-if-js"><?php echo __('Supplemental CSS editor'); ?></h3>
 <?php
 {
 	echo
 	'<form id="file-form" action="'.$p_url.'" method="post">'.
-	'<fieldset><legend>'.__('Supplemental CSS editor').'</legend>'.
 	'<p>'.form::textarea('css_content',72,25,html::escapeHTML($css_content),'maximal','',!$css_writeable).'</p>';
 	if ($css_writeable)
 	{
@@ -182,13 +152,62 @@ if ($dump != '') {
 	}
 	echo
 	'<p class="info">'.__('Note: this supplemental CSS rules will surcharge the default CSS rules.').'</p>'.
-	'</fieldset></form>';
+	'</form>';
 }
 ?>
 </div>
 
-<script>
+<div id="iconset"  class="multi-part" title="<?php echo __('Iconset management'); ?>">
+<h3 class="out-of-screen-if-js"><?php echo __('Iconset management'); ?></h3>
+<?php
+	echo
+	'<form id="iconset-form" action="'.$p_url.'" method="post">';
+	// Display list of iconset
+	if (count($iconsets)) {
+		$nb_actives = $nb_inactives = 0;
+		echo
+		'<div class="table-outer"><table id="iconset_list"><caption class="as_h3">'.__('Iconset list').'</caption>'.
+		'<thead>'.
+		'<tr>'."\n".
+		'  <th class="nowrap">'.__('Name').'</th>'."\n".
+		'  <th>'.__('Folder name').'</th>'."\n".
+		'  <th>'.__('Enabled').'</th>'."\n".
+		'  <th class="maximalx">'.__('Readme').'</th>'."\n".
+		'</tr>'."\n".
+		'</thead>'."\n".
+		'<tbody>';
+		foreach ($iconsets as $k => $v) {
+			echo
+			'<tr class="line">'.
+				'<td scope="row">'.form::checkbox(array('iconset[]'),$v['fname'],'','','',false,'title="'.__('select').' '.$v['name'].'"').' '.
+				$v['name'].'</td>'.
+				'<td>'.$v['fname'].'</td>'.
+				'<td>'.($v['enabled'] ? __('yes') : __('no')).'</td>'.
+				'<td>'.$v['freadme'].'</td>'.
+			'</tr>';
+			if ($v['enabled']) {
+				$nb_actives++;
+			} else {
+				$nb_inactives++;
+			}
+		}
+		echo '</tbody></table></div>';
+		echo
+		'<p>'.
+		'<input type="submit" name="enable" value="'.__('Enable').'" '.($nb_inactives ? '' : 'disabled="disabled"').' />'.' '.
+		'<input type="submit" name="disable" value="'.__('Disable').'" '.($nb_actives ? '' : 'disabled="disabled"').' />'.
+		'</p>';
+	}
+	echo
+	'<p>'.$core->formNonce().'</p>'.
+	'</form>';
+?>
+</div>
+
+<script type="text/javascript">
+//<![CDATA[
 	var editor = CodeMirror.fromTextArea(css_content, {mode: "css"});
+//]]>
 </script>
 </body>
 </html>
