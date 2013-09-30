0bb613f12cf36f0490fde7857ed058642eb83f2f
franck <carnet.franck.paul@gmail.com>
2013-09-30 12:13:59 +0200

    Cope with new breadcrumb

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
+// Get current content of CSS file
+
 $css_file = dirname(__FILE__).'/css/admin.css';
 $css_content = file_get_contents($css_file);
 $css_writeable = file_exists($css_file) && is_writable($css_file) && is_writable(dirname($css_file));
@@ -38,6 +40,62 @@ if (!empty($_POST['css'])) {
 		$core->error->add($e->getMessage());
 	}
 }
+
+// Get current list of iconsets
+// inactives have their names begining by a . (dash)
+ob_start();
+$iconsets = array();
+$iconsets_root = dirname(DC_RC_PATH).'/../admin/images/iconset/';
+$excluded_dirs = array('.hg','.git','.svn');
+var_dump($iconsets_root);
+if (is_dir($iconsets_root) && is_readable($iconsets_root)) {
+	if (($d = @dir($iconsets_root)) !== false) {
+		while (($entry = $d->read()) !== false) {
+			var_dump($entry);
+			if ($entry != '.' && $entry != '..' && is_dir($iconsets_root.'/'.$entry) && !in_array($entry, $excluded_dirs)) {
+				if (substr($entry, 0, 1) == '.') {
+					$name = substr($entry,1);
+					$enabled = false;
+				} else {
+					$name = $entry;
+					$enabled = true;
+				}
+				// Try to find a README.md, README.txt or README
+				$readme = '';
+				$freadme = '';
+				$path = $iconsets_root.'/'.$entry;
+				if (($f = @dir($path)) !== false) {
+					while (($file = $f->read()) !== false) {
+						if ($file != '.' && $file != '..' && !is_dir($path.'/'.$file)) {
+							if (in_array(strtolower($file), array('readme.md','readme.txt','readme'))) {
+								$freadme = $path.'/'.$file;
+								if (is_readable($freadme)) {
+									$readme = file_get_contents($freadme);
+									break;
+								} else {
+									$freadme = '';
+								}
+							}
+						}
+					}
+				}
+				$iconsets[$name] = array(
+					'name' => $name,
+					'fname' => $entry,
+					'enabled' => $enabled,
+					'freadme' => $freadme,
+					'readme' => html::escapeHTML($readme)
+				);
+				var_dump($iconsets[$name]);
+			}
+		}
+		// Sort array on iconset's name
+		ksort($iconsets);
+		var_dump($iconsets);
+	}
+}
+$dump = ob_get_clean();
+
 ?>
 
 <html>
@@ -55,13 +113,55 @@ if (!empty($_POST['css'])) {
 echo dcPage::breadcrumb(
 	array(
 		__('System') => '',
-		'<span class="page-title">'.__('Tidy administration settings').'</span>' => ''
+		__('Tidy administration settings') => ''
 	));
 if (!empty($_GET['css'])) {
 	dcPage::success(__('CSS supplemental rules updated'));
 }
 ?>
 
+<?php
+if ($dump != '') {
+//	echo '<div id="dump">'.$dump.'</div>';
+}
+?>
+
+<div id="iconset">
+<?php
+	echo
+	'<form id="iconset-form" action="'.$p_url.'" method="post">'.
+	'<fieldset><legend>'.__('Iconset management').'</legend>';
+	// Display list of iconset
+	if (count($iconsets)) {
+		$nb_actives = $nb_inactives = 0;
+		echo '<ul>';
+		foreach ($iconsets as $k => $v) {
+			echo
+			'<li>'.
+				__('name:').' '.$v['name'].' '.
+				__('fname:').' '.$v['fname'].' '.
+				__('enabled:').' '.($v['enabled'] ? __('yes') : __('no')).' '.
+				__('freadme:').' '.$v['freadme'].
+			'</li>';
+			if ($v['enabled']) {
+				$nb_actives++;
+			} else {
+				$nb_inactives++;
+			}
+		}
+		echo '</ul>';
+		echo
+		'<p>'.
+		'<input type="submit" name="enable" value="'.__('Enable').'" '.($nb_inactives ? '' : 'disabled="disabled"').' />'.' '.
+		'<input type="submit" name="disable" value="'.__('Disable').'" '.($nb_actives ? '' : 'disabled="disabled"').' />'.
+		'</p>';
+	}
+	echo
+	'<p>'.$core->formNonce().'</p>'.
+	'</fieldset>'.
+	'</form>';
+?>
+</div>
 <div id="css-editor">
 <?php
 {
