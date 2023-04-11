8434e75c9e38e85a65ecc8a9ac8fa6c897289b1f
franck <carnet.franck.paul@gmail.com>
2023-04-11 12:29:13 +0200

    l10n (add en)

---
+++ locales/en/main.lang.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Customize your dotclear administration'] = '';
+L10n::$locales['Tidy Administration'] = '';
+L10n::$locales['Options updated'] = '';
+L10n::$locales['Unable to write file %s. Please check the dotclear var folder permissions.'] = '';
+L10n::$locales['JS supplemental script updated'] = '';
+L10n::$locales['CSS supplemental rules updated'] = '';
+L10n::$locales['PO supplemental l10n updated'] = '';
+L10n::$locales['Tidy administration settings'] = '';
+L10n::$locales['System'] = '';
+L10n::$locales['Options'] = '';
+L10n::$locales['Use mini Dotclear icon (top left) in header'] = '';
+L10n::$locales['Move the search form (main menu) in header'] = '';
+L10n::$locales['Clone the media manager search input in always visible area'] = '';
+L10n::$locales['Enabled mouse hover activation on collapser'] = '';
+L10n::$locales['Move plugin settings link to top of page'] = '';
+L10n::$locales['Save'] = '';
+L10n::$locales['Supplemental CSS editor'] = '';
+L10n::$locales['Note: this supplemental CSS rules will surcharge the default CSS rules.'] = '';
+L10n::$locales['Sample CSS:'] = '';
+L10n::$locales['Supplemental JS editor'] = '';
+L10n::$locales['Note: this supplemental JS script will surcharge the default JS scripts.'] = '';
+L10n::$locales['Sample JS:'] = '';
+L10n::$locales['Supplemental PO editor'] = '';
+L10n::$locales['Note: this supplemental PO l10n will surcharge the default l10n.'] = '';
+L10n::$locales['Sample PO:'] = '';
