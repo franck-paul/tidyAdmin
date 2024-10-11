8e70e11fc85d3cc246ebfd9c368abd4c62f3f6a6
franck <carnet.franck.paul@gmail.com>
2024-10-11 16:29:30 +0200

    New release 8.6

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Tidy Administration',
     'Customize your dotclear administration',
     'Franck Paul',
-    '8.5',
+    '8.6',
     [
         'requires' => [['core', '2.31']],
         'type'     => 'plugin',
