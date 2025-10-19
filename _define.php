12f1efee174a4a00344069c818305aa4b5da57dc
franck <carnet.franck.paul@gmail.com>
2025-10-19 08:18:02 +0200

    New release 9.11

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Tidy Administration',
     'Customize your dotclear administration',
     'Franck Paul',
-    '9.10',
+    '9.11',
     [
-        'date'     => '2025-10-18T21:03:02+0200',
+        'date'     => '2025-10-19T08:18:02+0200',
         'requires' => [['core', '2.36']],
         'type'     => 'plugin',
         'settings' => [],
