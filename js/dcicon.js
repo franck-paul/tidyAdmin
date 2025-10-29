9efb5a4267a4cdc61a3f402a69b1024ba114e718
franck <carnet.franck.paul@gmail.com>
2025-10-29 09:08:17 +0100

    Apply mini-icon setting to popup windows too

--- js/dcicon.js
+++ js/dcicon.js
@@ -3,4 +3,10 @@
 
 dotclear.ready(() => {
   document.getElementById('header')?.classList.add('tidy_dcicon');
+  document.querySelector('body.popup')?.classList.add('tidy_dcicon');
+  const title = document.querySelector('body.popup h1');
+  if (title) {
+    title.setAttribute('title', title.textContent);
+    title.textContent = '';
+  }
 });
