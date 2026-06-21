0c3eaa44edfe6b0a5fa003e2425c0313d8b97db6
franck <contact@open-time.net>
2026-06-21 08:11:01 +0200

    New release 10.2.2

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Tidy Administration',
     'Customize your dotclear administration',
     'Franck Paul',
-    '10.2.1',
+    '10.2.2',
     [
-        'date'     => '2026-06-21T07:43:28+0200',
+        'date'     => '2026-06-21T08:11:01+0200',
         'requires' => [['core', '2.38']],
         'type'     => 'plugin',
         'settings' => [],
