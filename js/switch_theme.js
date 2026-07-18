d6816ff31b910ab48f022050434367b1d65de9fb
franck <contact@open-time.net>
2026-07-18 11:50:51 +0200

    Don't catch double-click event if not on header itself

--- js/switch_theme.js
+++ js/switch_theme.js
@@ -5,7 +5,8 @@ dotclear.ready(() => {
   // double click on header switch current theme (only if dotclear.debug is not set)
   if (!dotclear.debug) {
     const header = document.querySelector('#header') ? document.querySelector('#header') : document.querySelector('h1');
-    header?.addEventListener('dblclick', (_event) => {
+    header?.addEventListener('dblclick', (event) => {
+      if (event.target !== header) return;
       let { theme } = document.documentElement.dataset;
       if (theme == null || theme === '') {
         theme = globalThis.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
