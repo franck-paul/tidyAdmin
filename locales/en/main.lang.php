498b453aa04eade9f7fc55c111e8e06a6893ee03
franck <contact@open-time.net>
2026-08-19 05:46:45 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -53,6 +53,7 @@ L10n::$locales['Move plugin settings link to top of page'] = '';
 L10n::$locales['Always display editor toolbar during edition'] = '';
 L10n::$locales['Display direct access menus on blog parameters page'] = '';
 L10n::$locales['Display direct access menus on preferences page'] = '';
+L10n::$locales['Keep the list of defined widgets visible while scrolling (widget management page)'] = '';
 L10n::$locales['Save'] = '';
 L10n::$locales['Supplemental CSS'] = '';
 L10n::$locales['Note: this supplemental CSS rules will surcharge the default CSS rules.'] = '';
